ce8f87c27d21040dab2066fc053dca2e8722dd4f
Daniel <rschampextreme@gmail.com>
2022-10-27 16:00:41 +1300

    Add blank text

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function increaseCount(){
     count += 1;
     document.getElementById("count_num").innerHTML = count;
     console.log('clicked')
+    document.getElementById("blank_text").innerHTML = 'increaseCount';
 }
 
 function resetCount(){
@@ -16,4 +17,5 @@ function resetCount(){
 
 function increaseCount2(){
     intervalId = setInterval(increaseCount(),1000);
+    document.getElementById("blank_text").innerHTML = 'increaseCount2';
 }
